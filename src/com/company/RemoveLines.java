830a00be789b2fe079a2553fe285248d1c149e0a
Mudeesha Tharindu Dilshan <61771248+mudeesha@users.noreply.github.com>
2023-05-15 01:09:56 +0530

    finalized

---
+++ src/com/company/RemoveLines.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class RemoveLines extends JFrame {
+    private JPanel ManinPanel;
+    private JTextArea textArea1;
+    private JButton convertButton;
+    private JButton coppyButton;
+
+
+}
